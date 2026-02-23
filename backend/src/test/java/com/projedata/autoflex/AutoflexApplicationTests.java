e7e412b06722dff430977023614c14ad9ca96b19
lenonmerlo <lenontm@gmail.com>
2026-02-23 10:34:58 -0300

    chore: bootstrap backend module

---
+++ backend/src/test/java/com/projedata/autoflex/AutoflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projedata.autoflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
